1de3c88fe3f842e8a812c19908b6ead9864b00b5
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-04-03 04:33:10 +0530

    Add country code phone input

--- src/components/AddSubscriber.js
+++ src/components/AddSubscriber.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import IntlTelInput from "react-bootstrap-intl-tel-input";
+import phone from "phone";
 
 import { addSubscriber } from "../actions/subscriberActions";
 
 class AddSubscriber extends Component {
 	state = {
 		name: "",
-		phone: null
+		phone: ""
 	};
 
 	handleSubmit = e => {
 		e.preventDefault();
 		this.props.addSubscriber(this.state.name, this.state.phone);
-		this.setState({ name: "", phone: null });
+		this.setState({ name: "", phone: "" });
 		this.props.history.push("/");
 	};
 
 	onChange = e => {
 		this.setState({ [e.target.name]: e.target.value });
+	};
+
+	onPhoneChange = data => {
+		this.setState({ phone: phone(data.intlPhoneNumber)[0] });
 	};
 
 	render() {
@@ -37,12 +43,12 @@ class AddSubscriber extends Component {
 					</div>
 					<div className="form-group">
 						<label>Phone Number</label>
-						<input
-							type="number"
-							name="phone"
-							onChange={this.onChange}
-							className="form-control"
-							placeholder="Enter your phone no."
+						<IntlTelInput
+							preferredCountries={["IN"]}
+							defaultCountry={"IN"}
+							onChange={data => this.onPhoneChange(data)}
+							value={this.state.phone}
+							inputClassName={!this.state.phone && " is-invalid"}
 						/>
 					</div>
 					<button type="submit" className="btn btn-success">
